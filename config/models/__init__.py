e401f76f08914931ed7cf6fc12b62d8793707a62
Alex Pavlov <yukiu217@gmail.com>
2024-08-24 13:42:19 +0600

    create table post

--- config/models/__init__.py
+++ config/models/__init__.py
@@ -1,10 +1,12 @@
 __all__ = ('Base',
            'Product',
            'User',
+           'Post',
            'db_helper',
            )
 
 from .base import Base
 from .product import Product
 from .user import User
+from .post import Post
 from .db_helper import db_helper
